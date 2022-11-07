585fa372a9fe7118cb9f168d06f2f39b492bf104
brightlightkim <taeyangk95@naver.com>
2022-11-07 12:48:39 -0700

    change

---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import './navbar.css'
+
+export default function Navbar(){
+    const logo =  require("../static/logo.png")
+
+    return (
+
+        <div className="header">
+            <a className='logo_container' href='/'>
+                <img className='logo' src={logo} alt="Logo" />
+            </a>
+            <div className='menu'>
+                <a href="/about">About</a>
+                <a href="/contact">Contact Us</a>                    
+            </div>
+        </div>
+
+    )
+}
