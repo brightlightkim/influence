ae8934b6c8e50dac20848e8ae5b737d29095454c
brightlightkim <75151164+brightlightkim@users.noreply.github.com>
2022-11-11 23:44:07 -0700

    added icons

--- src/components/influencer_box/index.js
+++ src/components/influencer_box/index.js
@@ -1,9 +1,28 @@
 import './influencer.css'
 
-export default function influencer_box(data){
+export default function InfluencerBox(data){
     return(
         <div className='influencer_box_container'>
-
+            <a href={data.profile_url}>
+                <img className='profile_picture' src={data.profile_picture_url} />
+            </a>
+            <div className='text_data_container'>
+                <a href={data.profile_url}>{data.name}</a>
+                <a href={data.video_url}>{data.title}</a>
+                <p>Date: {data.date}</p>
+            </div>
+            <div className='data_container'>
+                <img src='' alt='views'/>
+            </div>
+            <div className='data_container'>
+                <img src='' alt='likes'/>
+            </div>
+            <div className='data_container'>
+                <img src='' alt='comments'/>
+            </div>
+            <div className='data_container'>
+                <img src='' alt='shares'/>
+            </div>
         </div>
     )
 }
