d346e0831031766f7b315d36a99443995f1532cf
brightlightkim <75151164+brightlightkim@users.noreply.github.com>
2022-11-12 10:25:57 -0700

    updated the backend code to get the views

--- backend/tiktok.py
+++ backend/tiktok.py
@@ -38,18 +38,34 @@ def response():
     profile_url = driver.find_element(By.XPATH, '//*[@id="app"]/div[2]/div[2]/div[1]/div[3]/div[1]/div[2]/div/a[1]').get_attribute("href")
     date = driver.find_element(By.XPATH, '//*[@id="app"]/div[2]/div[2]/div[1]/div[3]/div[1]/div[2]/div/a[2]/span[2]/span[2]')
     
+    views = 0
+    element = driver.find_element(By.XPATH, '//*[@id="app"]/div[2]/div[2]/div/div[2]/div[2]/div')
+    print(element)
+    for element in driver.find_elements(By.XPATH, '//*[@id="app"]/div[2]/div[2]/div/div[2]/div[2]/div'):
+        print(element)
+        title = element.find_element(By.XPATH, '/div[2]/a').get_attribute('title')
+        print(title)
+        if title[:20] == driver.title[:20]:
+            views = element.find_element(By.XPATH, '/div[1]/div/div/a/div/div[2]/strong').text
+            print(views)
+            
+    # '//*[@id="app"]/div[2]/div[2]/div/div[2]/div[2]/div' # Parent
+    # '//*[@id="app"]/div[2]/div[2]/div/div[2]/div[2]/div/div[1]' # Each Element of the children's id
+    # '//*[@id="app"]/div[2]/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/a' # Title is the get attribute of title
+    # '//*[@id="app"]/div[2]/div[2]/div/div[2]/div[2]/div/div[1]/div[1]/div/div/a/div/div[2]/strong' # The path for the views (first 25 characters is the parent)
     data = {
         'title': driver.title,
         'video_url': url,
         'name': influencer_name.text,
         'date': date.text,
+        'views': views,
         'likes': likes.text,
         'comments': comments.text,
         'shares': shares.text,
         'profile_picture_url': profile_picture_url,
         'profile_url': profile_url
     }
-    
+    driver.close()
     driver.quit()
     return json.dumps(data)
 
