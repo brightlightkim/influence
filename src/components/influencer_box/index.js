edc9138f1031d8d8a1cbd5f760a3ee5aa0a606e7
curtisrosenvall <70542777+curtisrosenvall@users.noreply.github.com>
2022-11-12 18:09:12 -0700

    Update index.js

--- src/components/influencer_box/index.js
+++ src/components/influencer_box/index.js
@@ -6,43 +6,43 @@ import './influencer.css'
 
 export default function InfluencerBox(data){
 
-<<<<<<< HEAD
-    // const data = {
-    //     comments: "20.8K",
-    //     date: "10-13",
-    //     likes: "1.9M",
-    //     money: "5000",
-    //     name: "campingdiary6",
-    //     profile_picture_url: "https://p19-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/5f0683ba796008ab4f375a26d533b4cd~c5_100x100.jpeg?x-expires=1668448800&x-signature=6Pv7sVTH5SC7xpYbZoKmhUyZVa8%3D",
-    //     profile_url: "https://www.tiktok.com/@campingdiary6",
-    //     shares: "30.1K",
-    //     title: "#camp #camping #camplife #outdoors",
-    //     video_url: "https://www.tiktok.com/@campingdiary6/video/7154136248477633834?is_from_webapp=v1",
-    //     views: 0
-    // }
-=======
->>>>>>> 350cf2ca06877f33d10be9d52a7b5a4cad23d315
+// <<<<<<< HEAD
+//     // const data = {
+//     //     comments: "20.8K",
+//     //     date: "10-13",
+//     //     likes: "1.9M",
+//     //     money: "5000",
+//     //     name: "campingdiary6",
+//     //     profile_picture_url: "https://p19-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/5f0683ba796008ab4f375a26d533b4cd~c5_100x100.jpeg?x-expires=1668448800&x-signature=6Pv7sVTH5SC7xpYbZoKmhUyZVa8%3D",
+//     //     profile_url: "https://www.tiktok.com/@campingdiary6",
+//     //     shares: "30.1K",
+//     //     title: "#camp #camping #camplife #outdoors",
+//     //     video_url: "https://www.tiktok.com/@campingdiary6/video/7154136248477633834?is_from_webapp=v1",
+//     //     views: 0
+//     // }
+// =======
+// >>>>>>> 350cf2ca06877f33d10be9d52a7b5a4cad23d315
     
     const likes_icon = <FontAwesomeIcon icon="fa-duotone fa-circle-heart" />
     const comment_icon = <FontAwesomeIcon icon="fa-solid fa-comment-dots" />
     const share_icon = <FontAwesomeIcon icon="fa-sharp fa-solid fa-share" />
 
     return(
-        <div className='influencer_box_container skeleton' >
+        <div className='influencer_box_container' >
             <a href={data.profile_url}>
                 <img className='profile_picture' src={data.profile_picture_url} alt={data.name}/>
             </a>
-            <div className='text_data_container_skeleton'>
+            <div className='text_data_container'>
                 <a href={data.profile_url}>{data.name}</a>
                 <a href={data.video_url}>{data.title}</a>
                 <p>Date: {data.date}</p>
-            </div>
+            </div>s
             <div className='skeleton skeleton-text'>
                 <div className='data_container'>
                 <FontAwesomeIcon icon={faPlay} />
                 {data.views}
             </div>
-            <div className='data_container skeleton'>
+            <div className='data_container'>
                 <FontAwesomeIcon icon={faHeart}/>
                 {data.likes}
             </div>
@@ -53,7 +53,8 @@ export default function InfluencerBox(data){
             <div className='data_container'>
                 <FontAwesomeIcon icon={faShare} />
                 {data.shares}
-            </div></div>
+            </div>
+            </div>
             
         </div>
     )
