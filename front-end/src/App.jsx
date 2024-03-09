16d70fb8044edc824fe48ecf7c900475365e5efd
brightlightkim <k2289@byu.edu>
2024-03-09 04:57:39 -0700

    update with ui and framework

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -29,15 +29,15 @@ const App = () => {
   return (
     <UserContext.Provider value={{ userAuth, setUserAuth }}>
       <Routes>
-        <Route path='/editor' element={<Editor />} />
-        <Route path='/editor/:blog_id' element={<Editor />} />
+        {/* <Route path='/editor' element={<Editor />} />
+        <Route path='/editor/:blog_id' element={<Editor />} /> */}
         <Route path='/' element={<Navbar />}>
           <Route index element={<HomePage />} />
           <Route path='/signin' element={<UserAuthForm type='sign-in' />} />
           <Route path='/signup' element={<UserAuthForm type='sign-up' />} />
-          <Route path='search/:query' element={<SearchPage />} />
-          <Route path='user/:id' element={<ProfilePage />} />
-          <Route path='blog/:blog_id' element={<BlogPage />} />
+          {/* <Route path='search/:query' element={<SearchPage />} /> */}
+          {/* <Route path='user/:id' element={<ProfilePage />} />
+          <Route path='blog/:blog_id' element={<BlogPage />} /> */}
           <Route path='*' element={<PageNotFound />} />
         </Route>
       </Routes>
